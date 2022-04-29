b7ff3c6fd44e483fa0c47602997f6d9613fd3e72
kalettuce <kjiangzk7@gmail.com>
2022-04-29 12:43:10 -0700

    removed debug line

--- source/Main.hx
+++ source/Main.hx
@@ -7,8 +7,5 @@ class Main extends Sprite {
     public function new() {
         super();
         addChild(new FlxGame(0, 0, PlayState));
-        if (true) {
-            return;
-        }
     }
 }
