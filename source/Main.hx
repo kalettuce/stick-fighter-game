1be884e52dfc4fe5ba0c5793ef7815908cedc153
kalettuce <kjiangzk7@gmail.com>
2022-04-29 12:41:31 -0700

    changed vscode settings to use 4-space indentation

--- source/Main.hx
+++ source/Main.hx
@@ -3,11 +3,12 @@ package;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
-class Main extends Sprite
-{
-	public function new()
-	{
-		super();
-		addChild(new FlxGame(0, 0, PlayState));
-	}
+class Main extends Sprite {
+    public function new() {
+        super();
+        addChild(new FlxGame(0, 0, PlayState));
+        if (true) {
+            return;
+        }
+    }
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,22 +1,28 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 
-class PlayState extends FlxState
-{
-	var text:FlxText;
+class PlayState extends FlxState {
+    var sprite:FlxSprite;
 
-	override public function create()
-	{
-		super.create();
-		text = new FlxText(0, 0, FlxG.width, "hello, world", 128);
-		add(text);
-	}
+    override public function create() {
+        super.create();
+        sprite = new FlxSprite(AssetPaths.sword_char__png);
+        sprite.x = FlxG.width / 2 - sprite.width / 2;
+        sprite.y = FlxG.height / 2 - sprite.height / 2;
+        add(sprite);
+    }
 
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
-	}
+    override public function update(elapsed:Float) {
+        super.update(elapsed);
+
+        if (FlxG.keys.pressed.A) {
+            --sprite.x;
+        } else if (FlxG.keys.pressed.D) {
+            ++sprite.x;
+        }
+    }
 }
