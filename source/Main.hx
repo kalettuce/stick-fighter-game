3482056ee4b9889de8aa046938d083921081769a
kalettuce <kjiangzk7@gmail.com>
2022-05-09 14:22:31 -0700

    removed the haxeflixel splash screen

--- source/Main.hx
+++ source/Main.hx
@@ -6,6 +6,6 @@ import openfl.display.Sprite;
 class Main extends Sprite {
     public function new() {
         super();
-        addChild(new FlxGame(0, 0, MenuState));
+        addChild(new FlxGame(0, 0, MenuState, 1, 60, 60, true));
     }
 }
