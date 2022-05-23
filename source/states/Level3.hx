1f9f3b8d34726c23c434c5729f09dacfcc71ab91
kalettuce <kjiangzk7@gmail.com>
2022-05-23 12:35:00 -0700

    Made level 3 into a placeholder level

--- source/states/Level3.hx
+++ source/states/Level3.hx
@@ -44,19 +44,31 @@ class Level3 extends FlxState {
 
         // add the terrain
         map = new FlxTilemap();
-        map.loadMapFromCSV("assets/levels/level3_terrain.csv", "assets/images/sf_level_tiles.png", 64, 64);
-        map.screenCenter();
+        map.loadMapFromCSV("assets/levels/level1_terrain.csv", "assets/images/sf_level_tiles.png", 64, 64);
         add(map);
 
+        var placeholderText = new FlxText(250, 350, 0, "", 40);
+        placeholderText.color = FlxColor.WHITE;
+        placeholderText.text = "Level 3 is work in progress...";
+        add(placeholderText);
+        placeholderText.centerOrigin();
+        placeholderText.screenCenter();
+
+        exitButton = new FlxButton(0, 0, "Return to Menu", exit);
+        exitButton.scale.set(2, 2);
+        exitButton.updateHitbox();
+        exitButton.label.setFormat(null, 13, FlxColor.BLACK);
+        exitButton.label.fieldWidth = exitButton.width;
+        exitButton.label.alignment = "center";
+        exitButton.label.offset.y -= 8;
+        exitButton.x = 1090;
+        exitButton.y = 20;
+        add(exitButton);
+        
         // add the player character
-        player = new Player();
+        /*player = new Player();
         player.setPosition(FlxG.width / 2 - player.width / 2, FlxG.height / 2 - player.height / 2);
         add(player);
-
-        exitButton = new FlxButton(0, 0, "Exit", exit);
-        exitButton.screenCenter(X);
-        exitButton.y = 10;
-        add(exitButton);
 
         // create health bar
         healthBar = new FlxBar(0, 0, LEFT_TO_RIGHT, 100, 10, player, "health", 0, 100, true);
@@ -79,7 +91,7 @@ class Level3 extends FlxState {
         nextLevel = Level4;
          
         haxe.Timer.delay(splash_screen_delay, 2000);
-
+        */
        
     }
 
@@ -93,7 +105,7 @@ class Level3 extends FlxState {
         FlxG.switchState(new MenuState());
      }
 
-    override public function update(elapsed:Float) {
+    /*override public function update(elapsed:Float) {
         var atkPressed:Bool = FlxG.keys.pressed.K;
         if (atkPressed) {
             if (player.health < -1)
@@ -121,7 +133,7 @@ class Level3 extends FlxState {
 
         super.update(elapsed);
         FlxG.collide(player.collider, map);
-    }
+    }*/
 
     private function splash_screen_delay() {
         Actuate.tween(levelScreen, 7, { alpha: 0 }); // fade out
